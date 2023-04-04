2f7d52639a298047c48c53f61e21abe949004f02
Kapil <sharmaakapil01@gmail.com>
2023-04-04 12:57:00 +0530

    Implemented the search functionality and completed the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import {useEffect,useState} from 'react';
 import ImageCard from './components/ImageCard';
-
+import ImageSearch from './components/ImageSearch';
 function App() {
 
   const [images,setImages] = useState([]);
   const [loading,setLoading] = useState(true);
+  const [term,setTerm] = useState('anime');
   useEffect(()=>{
-  fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&q=yellow+flowers&image_type=photo`)
+  fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&q=${term}&image_type=photo`)
   .then((res)=> res.json())
   .then((data)=> {
    setImages(data.hits);
    setLoading(false);
   })
-  .catch((e)=> console.log(e))},[]);
+  .catch((e)=> console.log(e))},[term]);
 
   return (
-    <div className="container mx-auto">
-      <div className="grid grid-cols-3 gap-4">
+    <div className="container mx-auto my-4">
+      <ImageSearch setText={(text)=>{setTerm(text)}}/>
+      
+        {!loading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found!!!</h1>}
+        {loading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+        :
+        <div className="grid grid-cols-3 gap-4">
         {
+          
           images.map((image,index)=> <ImageCard key={image.id+index} tags={image.tags} user={image.user} url={image.webformatURL} downloads={image.downloads} views={image.views} likes={image.likes}/>)
+    
         }
       </div>
+        }
+      
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,jsx,js}"],
   theme: {
     extend: {},
   },
